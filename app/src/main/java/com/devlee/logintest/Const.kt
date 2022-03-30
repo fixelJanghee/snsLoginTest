ac81710a61985adc19cfb36b5a5204aafd0eee4e
fixelJanghee <sj90947@fixelsoft.com>
2022-03-30 20:27:34 +0900

    - firebase 추가

---
+++ app/src/main/java/com/devlee/logintest/Const.kt
@@ -0,0 +1,6 @@
+package com.devlee.logintest
+
+object Const {
+    const val HOME = "68:D8:49:8F:85:1D:AA:F6:B0:F2:78:93:4E:7A:66:48:16:F3:48:24"
+    const val FIXEL = "C7:F0:BC:80:64:26:48:86:CD:EC:33:F5:FE:E8:74:69:35:14:57:E8"
+}
